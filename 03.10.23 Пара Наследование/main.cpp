322376c283b38c48b32a2fdb2a144f174772a717
MihailFasteks <fastakovskiy.mihail04@gmail.com>
2023-10-03 08:52:32 +0300

    Initial Commit

---
+++ 03.10.23 Пара Наследование/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  03.10.23 Пара Наследование
+//
+//  Created by Michalis on 03.10.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
